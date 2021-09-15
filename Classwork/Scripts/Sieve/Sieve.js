116c048634eb86174c1daa8f983000b4c4720bf3
Crl-Vlz <crlvlz0215@gmail.com>
2021-09-14 21:51:02 -0500

    Added basic HTML page to display the results

---
+++ Classwork/Scripts/Sieve/Sieve.js
@@ -0,0 +1,34 @@
+function sieve (n){
+
+    var primes = [];
+
+    var array = Array(10000001).fill(1);
+
+    for( var i = 2; i < n; i++){
+        if (array[i] == 1){
+            for (var j = i*2; j < n; j += i){
+                array[j] = 0;
+            }
+        }
+    }
+
+    array[0] = 0;
+    array[1] = 0;
+
+    for( var i = 0; i < n; i++){
+        if( array[i] == 1) primes.push(i + "\n");
+    }
+
+    return primes;
+
+};
+
+//Requieres an html tag with ID List
+function printPrimesToN(n){
+    var listPrimes = "";
+    var primes = sieve(n);
+    listPrimes = primes.toString();
+    document.getElementById("List").innerText = listPrimes;
+};
+
+document.onload = printPrimesToN(1000000);
